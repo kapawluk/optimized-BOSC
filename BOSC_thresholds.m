ef131f181bcf8160eecebc8d6231af99e044959c
KAPawluk <kpawluk@ualberta.ca>
2024-09-13 15:26:23 -0600

    Update BOSC_thresholds.m

--- BOSC_thresholds.m
+++ BOSC_thresholds.m
@@ -1,23 +1,24 @@
-%    This file is part of the Better OSCillation detection (BOSC) library.
-%
-%    The BOSC library is free software: you can redistribute it and/or modify
-%    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation, either version 3 of the License, or
-%    (at your option) any later version.
-%
-%    The BOSC library is distributed in the hope that it will be useful,
-%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%    GNU General Public License for more details.
-%
-%    You should have received a copy of the GNU General Public License
-%    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-%
-%    Copyright 2010 Jeremy B. Caplan, Adam M. Hughes, Tara A. Whitten
-%    and Clayton T. Dickson.
-
 function [powthresh,durthresh]=BOSC_thresholds(Fsample,percentilethresh,numcyclesthresh,F,meanpower)
 % version 2.0
+%
+% This file is part of the Better OSCillation detection (BOSC) library.
+%
+% The BOSC library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% The BOSC library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+%
+% Copyright 2010 Jeremy B. Caplan, Adam M. Hughes, Tara A. Whitten
+% and Clayton T. Dickson.
+%
 % This function calculates all the power thresholds and duration
 % thresholds for use with BOSC_detect.m to detect oscillatory episodes
 %

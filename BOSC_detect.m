208ab6793b62d636e3bb2fd0c5d2525f5b963808
KAPawluk <kpawluk@ualberta.ca>
2024-09-13 15:30:59 -0600

    Update BOSC_detect.m

--- BOSC_detect.m
+++ BOSC_detect.m
@@ -1,23 +1,22 @@
-%    This file is part of the Better OSCillation detection (BOSC) library.
-%
-%    The BOSC library is free software: you can redistribute it and/or modify
-%    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation, either version 3 of the License, or
-%    (at your option) any later version.
-%
-%    The BOSC library is distributed in the hope that it will be useful,
-%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%    GNU General Public License for more details.
-%
-%    You should have received a copy of the GNU General Public License
-%    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-%
-%    Copyright 2010 Jeremy B. Caplan, Adam M. Hughes, Tara A. Whitten
-%    and Clayton T. Dickson.
-
 function detected=BOSC_detect(b,powthresh,durthresh,Fsample)
-% version 2.0
+%
+% This file is part of the Better OSCillation detection (BOSC) library.
+%
+% The BOSC library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% The BOSC library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+%
+% Copyright 2010 Jeremy B. Caplan, Adam M. Hughes, Tara A. Whitten
+% and Clayton T. Dickson.
 %
 % This function detects oscillations based on a wavelet power
 % timecourse, b, a power threshold (powthresh) and duration
